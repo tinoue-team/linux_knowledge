b442e6212eb42725f46c2c8c536153028e088c5b
t_inoue <t.artwork.ceramica@gmail.com>
2024-10-20 01:42:26 +0900

    add: Astro DB の追加
    - 初期設定の追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://linuledge-test.netlify.app',
-    integrations: [preact()],
+    integrations: [preact(), db()],
 });
