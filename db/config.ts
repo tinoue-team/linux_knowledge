fc9380d45055406f32a479dbaf2e3e4573b327be
t_inoue <t.artwork.ceramica@gmail.com>
2024-10-20 12:43:11 +0900

    add(astro:db): database seed
    - fixme: seed.ts と データを呼び出している index.astro にて開発環境内で、import error となっている問題がある

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,51 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
 
 // https://astro.build/db/config
+const Command = defineTable({
+    columns: {
+        id: column.number({ primaryKey: true }),
+        command: column.text({ unique: true }),
+        description: column.text({ optional: true }),
+        // usage: column.text(),
+        // aliases: column.text({ array: true }),
+    },
+    indexes: {
+        commandIndex: { on: ['command'] },
+    },
+    // foreignKeys: [
+    //     {
+    //         columns: ['id'],
+    //         references: () => CommandScenario.columns.commandId,
+    //     },
+    // ],
+});
+
+const Scenario = defineTable({
+    columns: {
+        id: column.number({ primaryKey: true }),
+        scenario: column.text(),
+        description: column.text({ optional: true }),
+        // commands: column.text({ array: true }),
+    },
+    // foreignKeys: [
+    //     {
+    //         columns: ['id'],
+    //         references: () => CommandScenario.columns.scenarioId,
+    //     },
+    // ],
+});
+
+const CommandScenario = defineTable({
+    columns: {
+        id: column.number({ primaryKey: true }),
+        commandId: column.number({ references: () => Command.columns.id }),
+        scenarioId: column.number({ references: () => Scenario.columns.id }),
+    },
+    indexes: {
+        commandScenarioIndex: { on: ['commandId', 'scenarioId'] },
+    },
+});
+
 export default defineDb({
-    tables: {},
+    tables: { Command, Scenario, CommandScenario },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,21 @@
-import { db } from 'astro:db';
+import { Command, Scenario, db } from 'astro:db';
+// import { Command, Scenario } from './config';
 
 // https://astro.build/db/seed
 export default async function seed() {
-    // TODO
+    await db.insert(Command).values([
+        { command: 'ls', description: 'List directory contents' },
+        { command: 'chmod', description: 'Change file or directory permission' },
+    ]);
+    await db.insert(Scenario).values([
+        {
+            scenario: 'list-files',
+            description: 'List files in the current directory',
+        },
+    ]);
+    // CommandScenario の挿入
+    // await db.insert(CommandScenario).values({
+    //     commandId: command.id,
+    //     scenarioId: scenario.id,
+    // });
 }
