fbcd1a17a361393467340b9c5a5bc1e236ba3ade
t_inoue <t.artwork.ceramica@gmail.com>
2024-10-31 22:58:48 +0900

    add: expressive code のドキュメント踏まえて、さまざまな設定を追加
    - plugin の追加(コードブロック折りたたみ、行番号表示)
    - 伴うコンテンツページの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,52 +29,6 @@ export default defineConfig({
             //         autogenerate: { directory: 'blog' },
             //     },
             // ],
-            expressiveCode: {
-                expressiveCode: true,
-                type: 'none',
-                themes: ['dark-plus', 'light-plus'],
-                // frame: 'none',
-                defaultFrame: 'none',
-                frames: {
-                    // removeCommentsWhenCopyingTerminalFrames: 'code',
-                    // extractFileNameFromCode: false,
-                    // Example: Hide the "Copy to clipboard" button
-                    showCopyToClipboardButton: true,
-                    borderWidth: '0px', // ボーダーをなしに
-                    borderColor: 'transparent', // ボーダー色を透明に
-                    shadowColor: 'transparent', // シャドウ色を透明に
-                    backgroundColor: 'transparent', // 背景色を透明に
-                },
-                // frames: false,
-
-                defaultProps: {
-                    type: 'none',
-                    wrap: true,
-                    overridesByLang: {
-                        // Example: Override the background color for JavaScript
-                        // js: {
-                        //     backgroundColor: '#000',
-                        // },
-                    },
-                },
-
-                styleOverrides: {
-                    borderRadius: '0.5rem',
-
-                    // You can optionally override the plugin's default styles here
-                    frames: {
-                        // type: 'none',
-                        showCopyToClipboardButton: true,
-                        // shadowColor: '#124',
-                        borderWidth: '0px',
-                        borderColor: 'transparent',
-                        // シャドウをなしに
-                        shadowColor: 'transparent',
-                        // 背景色を透明に
-                        backgroundColor: 'transparent',
-                    },
-                },
-            },
         }),
     ],
     output: 'hybrid',
---
+++ ec.config.mjs
@@ -0,0 +1,54 @@
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+
+/** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
+export default {
+    expressiveCode: true,
+    type: 'none',
+    themes: ['dark-plus', 'light-plus'],
+    plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+    frames: {
+        // removeCommentsWhenCopyingTerminalFrames: 'code',
+        // extractFileNameFromCode: false,
+        // Example: Hide the "Copy to clipboard" button
+        showCopyToClipboardButton: true,
+        borderWidth: '0px', // ボーダーをなしに
+        borderColor: 'transparent', // ボーダー色を透明に
+        shadowColor: 'transparent', // シャドウ色を透明に
+        backgroundColor: 'transparent', // 背景色を透明に
+        codePaddingInline: '3rem',
+    },
+
+    defaultProps: {
+        type: 'none',
+        wrap: false,
+        tabWidth: 4,
+        showLineNumbers: false,
+        overridesByLang: {
+            // Example: Override the background color for JavaScript
+            // js: {
+            //     backgroundColor: '#000',
+            // },
+            // 'bash,sh': {
+            //     showLineNumbers: false,
+            // }
+        },
+    },
+
+    styleOverrides: {
+        borderRadius: '0.5rem',
+
+        // You can optionally override the plugin's default styles here
+        frames: {
+            // type: 'none',
+            showCopyToClipboardButton: true,
+            // shadowColor: '#124',
+            borderWidth: '0px',
+            borderColor: 'transparent',
+            // シャドウをなしに
+            shadowColor: 'transparent',
+            // 背景色を透明に
+            backgroundColor: 'transparent',
+        },
+    },
+};
